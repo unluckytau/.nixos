0f585583e08752f6909e68deba2a1c7d86b92952
tau-zeta <taufiqhaikal1808@gmail.com>
2026-06-18 18:41:18 +0800

    changed font & font size

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -10,15 +10,14 @@
 
 	programs.kitty = {
 		enable = true;
-		themeFile = "vague";
 		settings = {
 			font_family = "GeistMono Nerd Font";
-			font_size = "11";
+			font_size = "12";
 			bold_font = "auto";
 			italic_font = "auto";
 			bold_italic_font = "auto";
 			tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
-			window_padding_width = "10 40";
+			# window_padding_width = "10 40";
 			hide_window_decorations = "yes";
 			tab_bar_edge = "top";
 			tab_bar_style = "powerline";
