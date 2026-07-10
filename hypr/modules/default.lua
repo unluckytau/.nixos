fd14ba365fe58097f8ab6ced7c73465ff9ab363b
tfqhkl <taufiqhaikal1808@gmail.com>
2026-07-10 21:37:52 +0800

    monitor swith using dofile

--- hypr/modules/default.lua
+++ hypr/modules/default.lua
@@ -1,3 +1,10 @@
+-- displays
+local monitors_path = os.getenv("HOME") .. "/.local/state/hypr/monitors.lua"
+local ok, err = pcall(dofile, monitors_path)
+if not ok then
+	hl.print("Failed to load monitors.lua: " .. tostring(err))
+end
+
 -- startup apps
 hl.on("hyprland.start", function () 
 	hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 24")
