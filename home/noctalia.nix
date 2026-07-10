51ea4271ab3000feb9975e15dc1f7f0d729edfa3
tfqhkl <taufiqhaikal1808@gmail.com>
2026-07-11 01:43:56 +0800

    added ember palette for noctalia

--- home/noctalia.nix
+++ home/noctalia.nix
@@ -3,4 +3,5 @@
 {
 	programs.noctalia.enable = true;
 	xdg.configFile."noctalia/noctalia.toml".source = ../etc/noctalia.toml;
+	xdg.configFile."noctalia/palettes/ember-noctalia.json".source = ../etc/ember-noctalia.json;
 }
