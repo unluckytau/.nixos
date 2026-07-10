f9ee158d39af4a6b8c8090e10d25cc1da5c80ea9
tfqhkl <taufiqhaikal1808@gmail.com>
2026-07-10 22:28:58 +0800

    added actions in prep.sh

--- prep.sh
+++ prep.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p neovim git --run "bash ./install.sh"
+#!nix-shell -p neovim git --run "bash ./prep.sh"
 
 git clone https://github.com/unluckytau/.nixos.git
 cp /etc/nixos/hardware-configuration.nix /home/tau/.nixos/system/hardware-configuration.nix
+
+mkdir -p /home/tau/.local/state/hypr
+cp /home/tau/.nixos/etc/display.lua /home/tau/.local/state/hypr/
