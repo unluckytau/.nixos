5c7a029c4723501f78bd8887a2864c31d4d76344
tfqhkl <taufiqhaikal1808@gmail.com>
2026-08-04 15:05:07 +0800

    changed blur settings

--- etc/blur.lua
+++ etc/blur.lua
@@ -1,20 +1,16 @@
 -- -> ~/.local/state/hypr/
--- general blur
+
+-- hyprland blur
 hl.config({
 	decoration = {
 		blur = {
-			enabled = true,
-			brightness = 1.0,
-			contrast = 1.0,
-			noise = 0.01,
-			vibrancy = 0.2,
-			vibrancy_darkness = 0.5,
-			passes = 4,
-			size = 7,
-			popups = true,
-			popups_ignorealpha = 0.2,
-			new_optimizations = true,
-			-- ignore_opacity = true,
+      enabled = true,
+      size = 8,
+      passes = 3,
+      new_optimizations = true,
+      ignore_opacity = false,
+      vibrancy = 0.1696,
+      special = false,
 		},
 	},
 })
